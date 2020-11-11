9ced2879ba7f3fe3c08c8cd1a6d53b836b0e0537
waihinchan <waihinchan@icloud.com>
2020-11-11 15:12:13 +0000

    fixed make_pair script

--- main.py
+++ main.py
@@ -22,23 +22,23 @@ start_epoch = 1 if myoption.mode == 'train' else myoption.which_epoch
 print('start to train')
 for i in range(start_epoch,mymodel.opt.epoch):
     epoch_start_time = time.time()
-    print_loss = {'G_loss':0,'D_loss':0}
+    # print_loss = {'G_loss':0,'D_loss':0}
     for j, pair in enumerate(pair_data,start=1):
         loss,fake = mymodel(pair,myoption.forward)
         mymodel.set_requires_grad(mymodel.netD, False)
         G_loss = loss['G_loss']
-        print_loss[G_loss]+=G_loss
+        # print_loss[G_loss]+=G_loss
         mymodel.optimizer_G.zero_grad()
         G_loss.backward()
         mymodel.optimizer_G.step()
         mymodel.set_requires_grad(mymodel.netD, True)
         mymodel.optimizer_D.zero_grad()
         loss_D = loss['D_loss']
-        print_loss[D_loss]+=loss_D
+        # print_loss[D_loss]+=loss_D
         loss_D.backward()
         mymodel.optimizer_D.step()
-    print_loss['G_loss']=print_loss['G_loss']/j
-    print_loss['D_loss']=print_loss['D_loss']/j
+    # print_loss['G_loss']=print_loss['G_loss']/j
+    # print_loss['D_loss']=print_loss['D_loss']/j
     print('End of epoch %d / %d \t Time Taken: %d sec' %
           (i, myoption.epoch, time.time() - epoch_start_time))
     if i >= mymodel.opt.niter_decay:
@@ -46,7 +46,7 @@ for i in range(start_epoch,mymodel.opt.epoch):
         mymodel.update_learning_rate(updateepoch)
     if i % 5 == 0:
         print('epoch%s last loss is %s' % (i, loss))
-        print('epoch%s average loss is %s' % (i, print_loss))
+        # print('epoch%s average loss is %s' % (i, print_loss))
     if i % 5 == 1:
         print('epoch%s last loss is %s' % (i, loss))
         mymodel.opt.save_result = False
